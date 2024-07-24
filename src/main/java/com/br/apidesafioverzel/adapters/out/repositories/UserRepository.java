8397cd16ad7ac611bfbfecf868fdac13ec2c274d
joaomgsa <joaomgsa@gmail.com>
2024-07-24 00:22:51 -0300

    spring security

--- src/main/java/com/br/apidesafioverzel/adapters/out/repositories/UserRepository.java
+++ src/main/java/com/br/apidesafioverzel/adapters/out/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.br.apidesafioverzel.adapters.out.repositories;
 
 import com.br.apidesafioverzel.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
